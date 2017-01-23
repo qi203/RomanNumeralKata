8d1d95c97ca07dd1af99c299847c2effd9f425c6
qi203 <qi203@nyu.edu>
2017-01-22 20:03:11 -0500

    First Commit

---
+++ tests/CMakeFiles/check_money.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/check_money.dir/check_money.c.o"
+  "check_money.pdb"
+  "check_money"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/check_money.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
